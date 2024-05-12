f09c5a75844110050e7cae3c9e1e0050edb7db4d
Zhekicho1d <zheka5438@gmail.com>
2024-05-12 16:19:48 +0300

    reset commit

---
+++ products/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+@login_required
+def index(request):
+    return render(request,'products/index.html')
+
+@login_required
+def staff(request):
+    return render(request,'products/staff.html')
+
+@login_required
+def orders(request):
+    return render(request,'products/orders.html')
+
+@login_required
+def products(request):
+    return render(request,'products/products.html')
