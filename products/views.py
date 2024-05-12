41d8e22dd8d989cd1fdfcefbdee0b6236b1f4d4e
Zhekicho1d <zheka5438@gmail.com>
2024-05-12 22:38:00 +0300

    profile edtiting added

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Products
 
 # Create your views here.
 
