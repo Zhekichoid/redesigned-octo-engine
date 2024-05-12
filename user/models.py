469bd3a0298f84716eeb9ba1f6b353f2cf593ed9
Zhekicho1d <zheka5438@gmail.com>
2024-05-12 16:28:52 +0300

    git cache cleared

--- user/models.py
+++ user/models.py
@@ -2,11 +2,3 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Profile(models.Model):
-    staff = models.OneToOneField(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=200)
-    phone = models.CharField(max_length=20)
-    image = models.ImageField(default='avatar.jpg', upload_to='Profile_Images')
-
-    def __str__(self) -> str:
-        return f'{self.staff.username}-Profile'
